5c55d5c4af55d76e923ae9358c08112748bfdd38
Тимур Архипов <51990174+timur1arkhipov@users.noreply.github.com>
2022-05-29 18:55:18 +0400

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -10,13 +10,13 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export const fetchPost = (id) => API.get(`/posts/${id}`);
-export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
-export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
-export const createPost = (newPost) => API.post('/posts', newPost);
-export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const fetchPost = (id) => API.get(`https://trainee--application-server.herokuapp.com/posts/${id}`);
+export const fetchPosts = (page) => API.get(`https://trainee--application-server.herokuapp.com/posts?page=${page}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`https://trainee--application-server.herokuapp.com/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
+export const createPost = (newPost) => API.post('https://trainee--application-server.herokuapp.com/posts', newPost);
+export const likePost = (id) => API.patch(`https://trainee--application-server.herokuapp.com/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => API.patch(`https://trainee--application-server.herokuapp.com/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`https://trainee--application-server.herokuapp.com/posts/${id}`);
 
-export const signIn = (formData) => API.post('/user/signin', formData);
-export const signUp = (formData) => API.post('/user/signup', formData);
+export const signIn = (formData) => API.post('https://trainee--application-server.herokuapp.com/user/signin', formData);
+export const signUp = (formData) => API.post('https://trainee--application-server.herokuapp.com/user/signup', formData);
